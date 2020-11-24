1f4714ccc3ad26467bb10f81c0ca8fb743ea2eed
Dávid Balázs <modellmaker01@gmail.com>
2020-11-25 00:20:43 +0100

    homework 17

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
@@ -13,12 +13,37 @@ class Movies():
 egy = Movies("cim", "leiras", "IMDB")
 ketto = Movies("cim2", "leiras2", "IMDB2")
 harom = Movies("cim3", "leiras3", "IMDB3")
+movies = [egy, ketto, harom]
 
 
 @app.route("/")
 def index():
-    movies = [egy, ketto, harom]
-    return render_template("index.html", movies=movies)
+    return render_template("index.html")
+
+
+my_dict = {"asd@asd.asd": "asd", "joe@joe.joe": "joe", "email@address.com": "pwd1234"}
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if username in my_dict:
+        if my_dict[username] == password:
+            response = make_response(render_template("index.html", movies=movies, name=username))
+            response.set_cookie("c_username", username)
+    else:
+        response = render_template("index.html")
+
+    return response
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    response = make_response(render_template("index.html"))
+    response.set_cookie("c_username", expires=0)
+    return response
 
 
 if __name__ == "__main__":
